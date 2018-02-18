b8400d28526e401f9a9e7d23c55bc747ca8cb935
md-hamed <mohamed.hamed@espace.com.eg>
2018-02-18 02:37:02 +0200

    Add type to transaction to support STI

--- db/migrate/20180217222121_create_transactions.rb
+++ db/migrate/20180217222121_create_transactions.rb
@@ -4,6 +4,7 @@ class CreateTransactions < ActiveRecord::Migration[5.1]
       t.string :title
       t.decimal :amount, precision: 8, scale: 2, default: 0
       t.string :currency, default: 'USD'
+      t.string :type
       t.date :issued_on
       t.integer :scenario_id
 
