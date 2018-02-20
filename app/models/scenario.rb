259250390034f9f216b2f8cae1bbac63e1f0df63
md-hamed <mohamed.hamed@espace.com.eg>
2018-02-20 02:59:30 +0200

    Refactor cumulative_total to handle missing date

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -29,32 +29,16 @@ class Scenario < ApplicationRecord
   # if a month and year are provided; it returns cumulative
   # total till the provided date
   def cumulative_total(month = nil, year = nil)
-    first_available_income = incomes.minimum(:issued_on)
-    first_available_expense = expenses.minimum(:issued_on)
-
-    if month.present? && year.present?
-      date = Date.new(year, month, 1)
-      last_available_expense = last_available_income = date
-    else
-      last_available_income = incomes.maximum(:issued_on)
-      last_available_expense = expenses.maximum(:issued_on)
-    end
-
-    min_period_date = [first_available_income, first_available_expense].min
-    max_period_date = [last_available_income, last_available_expense].max
+    min_period_date, max_period_date = longest_period(month, year)
 
     # loop from min period date to max period date
     current_date = min_period_date
     cumulative_total = 0
     prev_month_income = 0
+    prev_month_expenses = 0
     while current_date != (max_period_date + 1.month) do
-      
-      month_income = revenue_on(current_date.month, current_date.year)
-
-      month_income = prev_month_income if month_income == 0
+      month_income, prev_month_income, prev_month_expenses = month_cumulative_total(prev_month_income, prev_month_expenses, current_date)
       cumulative_total += month_income
-      prev_month_income = month_income
-
       current_date += 1.month
     end
 
@@ -98,9 +82,10 @@ class Scenario < ApplicationRecord
   def add_transaction(type, amount, year, month)
     date = Date.new(year, month, 1)
     model = type.to_s.classify.constantize
+    money = Money.from_amount(amount)
 
     model.create(scenario: self,
-                 amount: amount,
+                 amount: money,
                  issued_on: date)
   end
 
@@ -109,12 +94,45 @@ class Scenario < ApplicationRecord
   def transactions_amount_on(type, month, year)
     model = type.to_s.classify.constantize
 
-    model.where('extract(month from issued_on) = ?', month)
-         .where('extract(year from issued_on) = ?', year)
-         .sum(:amount)
+    amount = model.where(scenario: self)
+                  .where('extract(month from issued_on) = ?', month)
+                  .where('extract(year from issued_on) = ?', year)
+                  .sum(:amount_cents)
+    Money.new amount
   end
 
+  # helper method to return true available
+  # transactions cumulative revenue
   def true_cumulative_revenue
-    incomes.sum(:amount) - expenses.sum(:amount)
+    incomes.sum(:amount_cents) - expenses.sum(:amount_cents)
+  end
+
+  # helper method to return lonest available
+  # period of time where transactions exist
+  def longest_period(month = nil, year = nil)
+    first_transaction_date = transactions.minimum(:issued_on)
+
+    if month.present? && year.present?
+      last_transaction_date = Date.new(year, month, 1)
+      if last_transaction_date < first_transaction_date
+        raise ArgumentError, "Provided date #{last_transaction_date} can not be less than first transaction date #{first_transaction_date}"
+      end
+    else
+      last_transaction_date = transactions.maximum(:issued_on)
+    end
+
+    [first_transaction_date, last_transaction_date]
+  end
+
+  # helper method to return month cumulative
+  # total given the knwoledge of previuos month
+  def month_cumulative_total(prev_month_income, prev_month_expenses, date)
+    income = income_on(date.month, date.year)
+    expenses = expenses_on(date.month, date.year)
+
+    income = prev_month_income if income.zero?
+    expenses = prev_month_expenses if expenses.zero?
+
+    [income - expenses, income, expenses]
   end
 end
