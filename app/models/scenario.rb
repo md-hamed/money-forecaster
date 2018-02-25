782cad7dab1b425295beb8532398776348f85f90
md-hamed <mohamed.hamed@espace.com.eg>
2018-02-25 02:05:33 +0200

    Re-implement Scenario model given forecasting/one-time transactions

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -13,12 +13,12 @@ class Scenario < ApplicationRecord
   # Getters
 
   def income_of_month(month, year, percent: 0.0)
-    amount = last_available_transaction_amount(:income, month, year)
+    amount = total_recurrent_amount_of_month(:income, month, year) + total_non_recurrent_amount_of_month(:income, month, year)
     forecasting?(month, year) ? raised_amount(amount, percent, month, year) : amount
   end
 
   def expenses_of_month(month, year, percent: 0.0)
-    amount = last_available_transaction_amount(:expense, month, year)
+    amount = total_recurrent_amount_of_month(:expense, month, year) + total_non_recurrent_amount_of_month(:expense, month, year)
     forecasting?(month, year) ? raised_amount(amount, percent, month, year) : amount
   end
 
@@ -32,20 +32,40 @@ class Scenario < ApplicationRecord
   # if a month and year are provided; it returns cumulative
   # total till the provided date
   def cumulative_total(month = nil, year = nil, income_percent: 0.0, expenses_percent: 0.0)
-    gaps_total = total_gaps_amount(:income, month, year, percent: income_percent) - total_gaps_amount(:expenses, month, year, percent: expenses_percent)
-    total_cumulative_transactions(month, year) + gaps_total
+    total_income = cumulative_recurrent_amount(:income, month, year, percent: income_percent) + cumulative_non_recurrent_amount(:income, month, year, percent: income_percent)
+    total_expenses = cumulative_recurrent_amount(:expense, month, year, percent: expenses_percent) + cumulative_non_recurrent_amount(:expense, month, year, percent: expenses_percent)
+
+    total_income - total_expenses
   end
 
   alias bank_balance cumulative_total
 
   # Actions
 
-  def add_income(amount, month, year, title = nil)
-    add_transaction(:income, amount, month, year, title)
+  def add_income(amount, month, year, title = nil, ending_month: nil, ending_year: nil)
+    add_transaction(:income, amount, month, year, title, 
+                    ending_month: ending_month, ending_year: ending_year)
   end
 
-  def add_expense(amount, month, year, title = nil)
-    add_transaction(:expense, amount, month, year, title)
+  def add_expense(amount, month, year, title = nil, ending_month: nil, ending_year: nil)
+    add_transaction(:expense, amount, month, year, title, 
+                    ending_month: ending_month, ending_year: ending_year)
+  end
+
+  def add_transaction(type, amount, month, year, title = nil, ending_month: nil, ending_year: nil)
+    date = Date.new(year, month, 1)
+    model = type.to_s.classify.constantize
+    money = Money.from_amount(amount)
+
+    if ending_month.present? && ending_year.present?
+      ending_date = Date.new(ending_year, ending_month)
+    end
+
+    model.create(scenario: self,
+                 amount: money,
+                 ending_on: ending_date,
+                 issued_on: date,
+                 title: title)
   end
 
   def duplicate
@@ -59,107 +79,71 @@ class Scenario < ApplicationRecord
   end
 
   def last_transaction_date
-    transactions.maximum(:issued_on)
+    @last_transaction_date ||= transactions.maximum(:issued_on)
   end
   
   private
 
-  def last_available_transaction_amount(type, month, year)
-    model = type.to_s.classify.constantize
-
-    last_transaction_date = model.where(scenario: self)
-                                 .where('issued_on <= ?', Date.new(year, month))
-                                 .last.try(:issued_on)
-    if last_transaction_date.present?
-      amount = model.where(scenario: self)
-                       .where(issued_on: last_transaction_date)
-                       .sum(:amount_cents)
-    end
-    
-    Money.new amount
+  def total_recurrent_amount_of_month(type, month, year)
+    Money.new recurrent_transactions_of_month(type, month, year).sum(:amount_cents)
   end
 
-  def add_transaction(type, amount, month, year, title = nil)
+  def total_non_recurrent_amount_of_month(type, month, year)
+    Money.new non_recurrent_transactions_of_month(type, month, year).sum(:amount_cents)
+  end
+
+  def recurrent_transactions_of_month(type, month, year)
+    model = type.to_s.classify.constantize
     date = Date.new(year, month, 1)
-    model = type.to_s.classify.constantize
-    money = Money.from_amount(amount)
 
-    model.create(scenario: self,
-                 amount: money,
-                 issued_on: date,
-                 title: title)
+    model.recurrent
+         .where(scenario: self)
+         .where('ending_on >= ?', date)
+         .where('issued_on <= ?', date)
   end
 
-  def total_transactions_of_month(type, month, year)
+  def non_recurrent_transactions_of_month(type, month, year)
     model = type.to_s.classify.constantize
+    date = Date.new(year, month, 1)
 
-    amount = model.where(scenario: self)
-                  .where('issued_on = ?', Date.new(year, month, 1))
-                  .sum(:amount_cents)
-    Money.new amount
+    model.non_recurrent.where(scenario: self)
+                       .where(issued_on: date)
   end
 
-  # get true available transactions cumulative total
-  # till given month/year
-  def total_cumulative_transactions(month, year)
-    income_total = incomes
-    expenses_total = expenses
-    
+  def cumulative_recurrent_amount(type, month = nil, year = nil, percent: 0.0, include_known_months: true)
     if month.present? && year.present?
-      income_total = income_total.where('issued_on <= ?', Date.new(year, month, 1))
-      expenses_total = expenses_total.where('issued_on <= ?', Date.new(year, month, 1))
-    end
-
-    Money.new(income_total.sum(:amount_cents) - expenses_total.sum(:amount_cents))
-  end
-
-  def total_gaps_amount(type, month = nil, year = nil, percent: 0.0)
-    model = type.to_s.classify.constantize
-    available_transactions = model.where(scenario: self).map(&:issued_on)
-    available_dates = dates_with_transactions_available(type, month, year)
-    
-    total_gaps_amount = 0
-    available_dates.each_cons(2).each do |prev, curr|
-      curr_contains_transaction = available_transactions.include?(curr)
-
-      if ((prev + 1.month) != curr) || !curr_contains_transaction # gap identified
-        gap_length = months_difference(curr, prev)
-        gap_length -= 1 if curr_contains_transaction
-        total_transactions = total_transactions_of_month(type, prev.month, prev.year)
-
-        if forecasting?(curr.month, curr.year) && !percent.zero?
-          forecasted_months_count = months_difference(curr, last_transaction_date)
-          non_forecasted_months = gap_length - forecasted_months_count
-          first_forecasted_month = last_transaction_date + 1.month
-
-          forecasted_months_count.times do |i|
-            total_gaps_amount += raised_amount(total_transactions, percent, (first_forecasted_month + i.months).month, (first_forecasted_month + i.months).year)
-          end
-          total_gaps_amount += non_forecasted_months * total_transactions
-        else
-          total_gaps_amount += gap_length * total_transactions
-        end
-      end
-    end
-
-    total_gaps_amount
-  end
-
-  # transactions dates till the provided month/year
-  def dates_with_transactions_available(type, month, year)
-    model = type.to_s.classify.constantize
-
-    available_dates = model.where(scenario: self)
-                           .order('issued_on ASC')
-
-    if month.present? && year.present?
-      end_date = Date.new(year, month)
+      date = Date.new(year, month, 1)
     else
-      end_date = last_transaction_date
+      date = last_transaction_date
     end
 
-    available_dates.where('issued_on <= ?', end_date)
-                     .map(&:issued_on).push(end_date).uniq
+    first_forecasted_month = last_transaction_date + 1.month
+    recurrent_transactions_of_month(type, date.month, date.year).sum do |transaction|
+      last_date_with_transactions_available = forecasting?(date.month, date.year) ? last_transaction_date : date
+
+      forecasted_months_count = months_difference(date, last_date_with_transactions_available)
+      non_forecasted_months_count = months_difference(last_date_with_transactions_available, transaction.issued_on) + 1
+
+      total_forecasted_amount = 0
+      forecasted_months_count.times do |i|
+        total_forecasted_amount += raised_amount(transaction.amount, percent, (first_forecasted_month + i.months).month, (first_forecasted_month + i.months).year)
+      end
+
+      include_known_months ? non_forecasted_months_count * transaction.amount + total_forecasted_amount : Money.new(total_forecasted_amount)
+    end
+  end
+
+  def cumulative_non_recurrent_amount(type, month = nil, year = nil, percent: 0.0)
+    model = type.to_s.classify.constantize
+    if month.present? && year.present?
+      date = Date.new(year, month, 1)
+    else
+      date = last_transaction_date
+    end
+
+    Money.new(model.non_recurrent.where(scenario: self)
+                   .where('issued_on <= ?', date)
+                   .sum(:amount_cents))
   end
 
   def months_difference(current_date, previous_date)
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -4,10 +4,27 @@ class Transaction < ApplicationRecord
   # Associations
   belongs_to :scenario
 
+  # Scopes
+  scope :recurrent, -> { where.not(ending_on: nil) }
+  scope :non_recurrent, -> { where(ending_on: nil) }
+
   # Validations
   validates :amount_cents, :issued_on, :type, presence: true
+  validate :ending_date_is_after_issue_date
 
   def signed_amount
     amount
+  end
+
+  def recurrant?
+    ending_on.present? && ending_on > issued_on
+  end
+
+  private
+
+  def ending_date_is_after_issue_date
+    if ending_on.present? && ending_on < issued_on
+      errors.add :ending_on, "can't be before issued date"
+    end
   end
 end
