87b07bafc13cd09ab98c6307728f0014a7c6cefc
md-hamed <mohamed.hamed@espace.com.eg>
2018-02-27 23:58:05 +0200

    Modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,26 +14,29 @@ scenario = user.scenarios.first_or_create(title: 'Scenario 1', current_date: Dat
 
 if scenario.transactions.none?
   puts 'Adding Transactions to Scenario 1..'
-  scenario.add_income(1000, 5, 2017, 'Salary 1', ending_month: 5, ending_year: 2030)
-  scenario.add_expense(500, 5, 2017, 'Serviec 1', ending_month: 5, ending_year: 2030)
+  scenario.add_income(1000, 5, 2017, 'Salary 1', payments_type: :monthly)
+  scenario.add_expense(500, 5, 2017, 'Serviec 1', payments_type: :monthly)
 
-  scenario.add_income(2000, 8, 2017, 'Salary 2', ending_month: 5, ending_year: 2030)
-  scenario.add_expense(1000, 8, 2017, 'Serviec 2', ending_month: 5, ending_year: 2030)
+  scenario.add_income(2000, 8, 2017, 'Salary 2', payments_type: :monthly)
+  scenario.add_expense(1000, 8, 2017, 'Serviec 2', payments_type: :monthly)
 end
 
-# cumulative total on 8, 2017 = 3000
-# cumulative total on 9, 2017 = 4500
-
-puts 'Seeding User ..'
-scenario = user.scenarios.create(title: 'Scenario 2')
+# cumulative total on 8, 2017 = $3,000
+# cumulative total on 9, 2017 = $4,500
 
 puts 'Creating Scenario 2 ..'
-scenario.add_income(1000, 5, 2017, 'Salary 1', ending_month: 5, ending_year: 2030)
-scenario.add_expense(500, 7, 2017, 'Service 1', ending_month: 5, ending_year: 2030)
+scenario = user.scenarios.create(title: 'Scenario 2', current_date: Date.new(2018, 2))
 
-puts 'Adding Transactions to Scenario 2..'
-scenario.add_income(2000, 10, 2017, 'Freelancing')
-scenario.add_expense(1000, 9, 2017, 'Serviec 2', ending_month: 5, ending_year: 2030)
+if scenario.transactions.none?
+  puts 'Adding Transactions to Scenario 2..'
+  scenario.add_income(1000, 11, 2017, 'Salary 1 (permanent)', payments_type: :monthly)
+  scenario.add_income(500, 11, 2017, 'Salary 2 (non-permanent)', ending_month: 5, ending_year: 2018, payments_type: :monthly)
+  scenario.add_income(1000, 12, 2017, 'Year bonus', payments_type: :yearly)
+  scenario.add_income(1000, 1, 2018, 'Salary 3', payments_type: :monthly)
 
-# cumulative total on 10, 2017 = 4000
-# cumulative total on 11, 2017 = 3500
+  scenario.add_expense(500, 11, 2017, 'Service 1', payments_type: :monthly)
+  scenario.add_expense(1000, 12, 2017, 'Laptop purchase', payments_type: :one_time)
+end
+
+# cumulative total on 2, 2018 = $6,000
+# cumulative total on 3, 2018 = $8075 (with 3% raise)
